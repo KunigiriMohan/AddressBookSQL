734a62985ae87f8a4115d1a846e893cd49450a31
KunigiMohan <kmohanr5@gmail.com>
2021-11-26 17:09:25 +0530

    Added : Inserting data into Address Book Table.

---
+++ UC3.sql
@@ -0,0 +1,7 @@
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no) values ('mohan','kunigiri','5/223','adoni','Andhra Pradesh',518301,8555058332);
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no) values ('Suresh','kumar','1/29','hyderabad','Telangana',500001,856559956);
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no) values ('mahesh','kumar','3/32','kurnool','Andhra Pradesh',518001,9562626226);
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no) values ('ramesh','reddy','5255/552','mumbai','Maharastra',400001,655666659);
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no) values ('vishnu','vsrdhan','5651/558','bangalore','karnataka',510001,7555882822);
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no) values ('Shravan','kumar','66/8899','ahemedabad','gujarat',300001,9262565465);
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no) values ('Sriman','sathya','8/9988','bangalore','karnataka',5100001,9565655626);
