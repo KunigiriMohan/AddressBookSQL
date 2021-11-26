114f6f40e1ed74103b42baeabf4f1db5a2839827
KunigiMohan <kmohanr5@gmail.com>
2021-11-26 17:07:00 +0530

    Added : Created DataBase.

---
+++ UC1.sql
@@ -0,0 +1,2 @@
+use Address_Book_Services;
+create table Address_Book_Table(first_name varchar(100),last_name varchar(100),address varchar(100),city varchar(100),state varchar(100),zip int ,phone_no double,primary key(phone_no));
