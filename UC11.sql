cfd372fe4033799048016b838612ed2ee0e0b8f9
KunigiMohan <kmohanr5@gmail.com>
2021-11-30 10:05:55 +0530

    Added :Ability to add person to Friend and Family.

---
+++ UC11.sql
@@ -0,0 +1,4 @@
+insert into address_book_table(first_name,last_name,address,city,state,zip,phone_no,type) values ('Shravan','kumar','5/223','adoni','Andhra Pradesh',518301,8555058332,'friends');
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no,Type) values ('Sai','kumar','5/223','adoni','Andhra Pradesh',518301,8555858332,'family');
+insert into Address_Book_Table(first_name,last_name,address,city,state,zip,phone_no,Type) values ('shiva','kumar','5/223','adoni','Andhra Pradesh',518301,8555058432,'friends');
+
